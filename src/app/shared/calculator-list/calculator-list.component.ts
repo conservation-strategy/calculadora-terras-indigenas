1843003b9e5bb0fccfc1bbe58ac08833278ee54e
Marcelo <marcelo@streetwire.net>
2024-04-12 12:06:44 -0400

    fixed links on calculator-list component

--- src/app/shared/calculator-list/calculator-list.component.ts
+++ src/app/shared/calculator-list/calculator-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-calculator-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './calculator-list.component.html',
   styleUrl: './calculator-list.component.scss'
 })
